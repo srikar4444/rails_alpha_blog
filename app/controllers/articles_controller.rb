7844a8a1199d8e21582271f29a0e2bdb55d8e54d
srikar chittampalli <srikarchittampalli@srikars-MacBook-Pro.local>
2020-03-03 13:29:53 +0530

    addition of new, create, show of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,21 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        # @article.save
+        # redirect_to articles_show(@article)
+        if @article.save
+            # do something
+            flash[:notice] = "Article is successfully created"
+            redirect_to article_path(@article)
+        else
+            # do something
+            render 'new'
+        end
+
     end
 
     def show
+        @article = Article.find(params[:id])
     end
 
     private
