eb2afc0a49b763ac4b26aab0a30cb9517e388fd1
srikar chittampalli <srikarchittampalli@srikars-MacBook-Pro.local>
2020-03-03 15:23:51 +0530

    addition of changes index page and changes to edit, show, new pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,19 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+
+
     def new 
         @article = Article.new
     end
-    
+
+    def edit 
+        @article = Article.find(params[:id])
+    end
+
+
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
@@ -19,9 +30,23 @@ class ArticlesController < ApplicationController
 
     end
 
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article is successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+
+    end
+
+
     def show
         @article = Article.find(params[:id])
     end
+
+
 
     private
         def article_params
